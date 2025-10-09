84d83f799bebaae40af18ffea374ea21ae41513d
Chetan291017 <chetan291017@hotmail.com>
2025-10-09 22:23:07 +0530

    Update main.tf for Azure resource

--- main.tf
+++ main.tf
@@ -1,27 +1,39 @@
 terraform {
   required_providers {
-    null = {
-      source  = "hashicorp/null"
-      version = "~> 3.2"
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.112"
     }
   }
 }
 
-provider "null" {}
+provider "azurerm" {
+  features {}
 
-resource "null_resource" "create_file" {
-  connection {
-    type        = "ssh"
-    host        = var.target_host
-    user        = var.ssh_user
-    private_key = file(var.private_key_path)
-  }
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+}
 
-  provisioner "remote-exec" {
-    inline = [
-      "echo '✅ File created by Terraform CI/CD workflow' > /tmp/terraform_ci_test.txt",
-      "hostname",
-      "ls -l /tmp/terraform_ci_test.txt"
-    ]
-  }
+# Resource Group
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-terraform-demo"
+  location = "East US"
+}
+
+# Virtual Network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet-demo"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+# Subnet
+resource "azurerm_subnet" "subnet" {
+  name                 = "subnet-demo"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
 }
