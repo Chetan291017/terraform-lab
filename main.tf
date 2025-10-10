dba472937e6586dfb29fbe178c6bfd8db32b952b
Chetan291017 <chetan291017@hotmail.com>
2025-10-10 07:47:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,12 +10,6 @@ terraform {
 provider "azurerm" {
   features {}
 
-  subscription_id = var.SUBSCRIPTION_ID
-  client_id       = var.CLIENT_ID
-  client_secret   = var.CLIENT_SECRET
-  tenant_id       = var.TENANT_ID
-}
-
 # Resource Group
 resource "azurerm_resource_group" "rg" {
   name     = "rg-terraform-demo"
