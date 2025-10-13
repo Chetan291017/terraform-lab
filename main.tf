c3467fc14228dde194ea2b522b16d2891d4c3419
Chetan291017 <chetan291017@hotmail.com>
2025-10-14 00:35:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,8 +6,8 @@ resource "azurerm_resource_group" "rg" {
 
 resource "azurerm_storage_account" "chetan_sa" {
   name                     = "ck291017" # Must be globally unique and lowercase
-  resource_group_name      = azurerm_resource_group.example_rg.name
-  location                 = azurerm_resource_group.example_rg.location
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS" # Locally Redundant Storage (choose based on requirements)
 
